4d9a895a4e35c4bff20332fe10a95d4b8a206600
Yohanes Damenta <gnitnigdy@gmail.com>
2025-05-13 05:10:53 +0700

    add: get data from reqres api for the first time

---
+++ mini-project-damenta/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const onLoadFetchData = async () => {
+      const response = await fetch("https://reqres.in/api/users?page=", {
+        headers: {
+          "x-api-key": "reqres-free-v1",
+        },
+      });
+
+      const data = await response.json();
+      console.log(data.data);
+    };
+    onLoadFetchData();
+  }, []);
+
+  return <div>Hello World</div>;
+}
+
+export default App;
